b3d1bd9bd39fe2c7293917fa17e8ecf0ac02aaf9
Ikersfletch <ikersfletch@gmail.com>
2018-12-18 11:09:57 -0600

    Update keys.js

--- nes/js/keys.js
+++ nes/js/keys.js
@@ -1,13 +1,21 @@
 var keys = [];
 var pkey = [];
 var rkey = [];
-
+var whichKey = null;
+var mouseFrame = false;
+mousePressed = function() {
+  mouseFrame = false;
+  if (mouseIsPressed === true) {
+    mouseFrame = true;
+  }
+}
 keyPressed = function() {
     pkey[keyCode]  = 0;
     if (keys[keyCode] === false) {
       pkey[keyCode]  ++;
     }
     keys[keyCode] = true;
+    whichKey = keyCode;
     if (keys[keyCode] === true) {
       pkey[keyCode] ++;
     }
@@ -19,6 +27,7 @@ keyReleased = function() {
       rkey[keyCode] ++;
     }
     keys[keyCode] = false;
+    whichKey = null;
     if (keys[keyCode] === false) {
       rkey[keyCode] ++;
     }
